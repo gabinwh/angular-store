5c06ebd0913adbcebaec01f3b639690a113b134e
Gabriel S <gcsprog@gmail.com>
2025-08-25 17:48:45 -0300

    chore: remover arquivos inuteis

--- src/app/features/admin/admin-module.ts
+++ src/app/features/admin/admin-module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { Admin } from './admin';
 import { ProductTableComponent } from './pages/product-table-component/product-table-component';
 
 
