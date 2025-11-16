a145f42640b14f612b030b80af3697aeac31f858
Gabriel S <gcsprog@gmail.com>
2025-11-16 12:19:55 -0300

    chore: ajustar problemas do merge

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
    {
     path: 'admin',
     canActivate: [authGuard],
-    loadChildren: () => import('./features/admin/admin-module').then(m => m.AdminModule),
+    loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule),
   },
 
   { path: '**', redirectTo: '/home' }
